d20c56c6d1bb3c74ad81756b367a9a4757e7275e
Jicheng Zhi <jzhi001@fiu.edu>
2019-06-26 10:56:59 +0800

    fin dp: edit-distance

--- 72.edit-distance.java
+++ 72.edit-distance.java
@@ -10,6 +10,21 @@ class Solution {
         int[][] grid = new int[src.length() + 1][dest.length() + 1];
 
         grid[0][0] = 0;
+
+        for(int r = 0; r <= src.length(); r++){
+            for(int c = 0; c <= dest.length(); c++){
+                if(r == 0 && c == 0) grid[r][c] = 0;
+                else if(r == 0) grid[r][c] = c;
+                else if(c == 0) grid[r][c] = r;
+                else if(src.charAt(r - 1) == dest.charAt(c - 1)) grid[r][c] = grid[r-1][c-1];
+                else{
+                    int tmp = Math.min(grid[r-1][c-1], grid[r-1][c]);
+                    grid[r][c] = Math.min(tmp, grid[r][c-1]) + 1;
+                }
+            }
+        }
+
+        return grid[src.length()][dest.length()];
     }
 }
 
