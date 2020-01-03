5f8f70a1c2834774b26f9eccef455eed551e166b
JichengZhi <zhijicheng@tuniu.com>
2020-01-03 13:53:53 +0800

    #718 DP

--- No718.java
+++ No718.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int findLength(int[] A, int[] B) {
+class No718 {
+    public int findLength_bruteForce(int[] A, int[] B) {
         if(A == null || B == null) return 0;
 
         int result = 0;
@@ -13,6 +13,27 @@ class Solution {
                     n++;
                 }
                 result = Math.max(result, n);
+            }
+        }
+
+        return result;
+    }
+
+    public int findLength_dp(int[] A, int[] B) {
+        if(A == null || B == null) return 0;
+
+        int[][] memo = new int[A.length + 1][B.length + 1];
+
+        int result = 0;
+
+        for(int i = A.length - 1; i >= 0; i--){
+            for(int j = B.length - 1; j >= 0; j--){
+                if(A[i] == B[j]){
+                    memo[i][j] = memo[i+1][j+1] + 1;
+                }else{
+                    memo[i][j] = 0;
+                }
+                result = Math.max(result, memo[i][j]);
             }
         }
 
