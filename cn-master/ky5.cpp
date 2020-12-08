f16f1bf6451a759f6dd6800f866da97db0bb3666
Jicheng Zhi <jichengzhi0716@icloud.com>
2020-12-09 00:01:06 +0800

    ky 5

---
+++ cn-master/ky5.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    
+    string s;
+    while(cin >> s){
+        reverse(s.begin(), s.end());
+        cout << s << endl;
+    }
+    
+    return 0;
+}
