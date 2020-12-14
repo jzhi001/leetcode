67982e1b8e86cc2c9667f73164788c6281e6c164
Jicheng Zhi <jichengzhi0716@icloud.com>
2020-12-14 22:36:16 +0800

    ky 164

---
+++ cn-master/ky164.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string a, b;
+
+    while(cin >> a >> b){
+
+        string ans = a + b;
+
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
