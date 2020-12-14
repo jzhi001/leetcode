8d1e906cdd9bbffbd9b925f8cd02b3dfc9fbef22
Jicheng Zhi <jichengzhi0716@icloud.com>
2020-12-14 22:53:53 +0800

    ky 168

---
+++ cn-master/ky168.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+    string s;
+
+    while(cin >> s){
+
+        sort(s.begin(), s.end());
+
+        cout << s << endl;
+    }
+
+    return 0;
+}
