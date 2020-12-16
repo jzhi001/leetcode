7d030c70f3495362a411f2ece9e5b5c7a688dd36
Jicheng Zhi <jichengzhi0716@icloud.com>
2020-12-16 11:51:53 +0800

    ky 271

---
+++ cn-master/ky271.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+    for(int a = 0; a <= 9; a++){
+        for(int b = 0; b <= 9; b++){
+            for(int c = 0; c <= 9; c++){
+                int x = a * 100 + b * 10 + c;
+                int y = b * 100 + c * 10 + c;
+
+                if(x + y == 532) printf("%d %d %d\n", a, b, c);
+            }
+        }
+    }
+
+    return 0;
+}
