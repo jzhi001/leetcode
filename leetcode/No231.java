a75a40b3bbe1d8c346623d7af590b26fed445e29
Zhi Jicheng <zhijicheng14557@ipalfish.com>
2021-01-05 11:21:35 +0800

    remove java solutions

--- leetcode/No231.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        for(int i=0; i<32; i++){
-            if(n == 1<<i) return true;
-        }
-        return false;
-    }
-}
